f2791c3d257afa3719fc80653d0f1cbe7d6defe5
artificialcitizens4 <artificialcitizens4@grappus.com>
2025-06-09 13:43:52 +0530

    Start repository

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="w-full h-screen">
+      <iframe 
+        src="/battlefield-editor-updated.html" 
+        className="w-full h-full border-0"
+        title="Battlefield Map Editor"
+      />
+    </div>
+  );
+}
+
+export default App;
